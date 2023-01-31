7da80ed962c0cb09fbb68e9c0fbe6fc68da76983
SHIHS <stevenshih@25sprout.com>
2023-01-31 23:04:09 +0800

    LeetCode(#1 TwoSum): Solved the problem

---
+++ src/Array/TestTwoSum1.java
@@ -0,0 +1,34 @@
+package Array;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestTwoSum1 {
+
+    @Test
+    public void twoSumTest1() {
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+        int[] expected = {0, 1};
+        int[] actual = TwoSum1.twoSum(nums, target);
+        Assert.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void twoSumTest2() {
+        int[] nums = {3, 3};
+        int target = 6;
+        int[] expected = {0, 1};
+        int[] actual = TwoSum1.twoSum(nums, target);
+        Assert.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void twoSumTest3() {
+        int[] nums = {3, 2, 4};
+        int target = 6;
+        int[] expected = {1, 2};
+        int[] actual = TwoSum1.twoSum(nums, target);
+        Assert.assertArrayEquals(expected, actual);
+    }
+}
---
+++ src/Array/TwoSum1.java
@@ -0,0 +1,51 @@
+package Array;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * LeetCode #1
+ * @author SHIHS 2023/1/31
+ * @resource <a href="https://leetcode.com/problems/two-sum/solutions/127810/two-sum/">...</a>
+ */
+public class TwoSum1 {
+    /**
+     * By using double for loops to traverse the array. The second array checks
+     * the elements excluding the first array and compares with the expected number.
+     * If true, add the first array [i] item and the second array [i] item to the list.
+     *
+     * @param nums
+     * @param target
+     * @return
+     */
+    public static int[] twoSum(int[] nums, int target) {
+        int len = nums.length;
+        for (int now = 0; now < len; now += 1) {
+            for (int compare = now + 1; compare < len; compare += 1) {
+                if (nums[compare] + nums[now] == target) {
+                    return new int[]{now, compare};
+                }
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Iterating elements into hash table, and also look back to check if current element's complement
+     * already exists in the hash table.
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int[] twoSumWithHash(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                return new int[] { map.get(complement), i };
+            }
+            map.put(nums[i], i);
+        }
+        return null;
+    }
+}
