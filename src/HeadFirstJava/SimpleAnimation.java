01e6480ff2447193a5c719d3feeca5e8bed54489
SHIHS <stevenshih@25sprout.com>
2023-02-16 15:28:09 +0800

    HeadFirstJava: P.494

---
+++ src/HeadFirstJava/SimpleAnimation.java
@@ -0,0 +1,45 @@
+package HeadFirstJava;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.concurrent.TimeUnit;
+public class SimpleAnimation {
+    private int xPos = 70;
+    private int yPos = 70;
+    public static void main(String[] args) {
+        SimpleAnimation gui = new SimpleAnimation();
+        gui.go();
+    }
+    public void go() {
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        MyDrawPanel drawPanel = new MyDrawPanel();
+        frame.getContentPane().add(drawPanel);
+        frame.setSize(300, 300);
+        frame.setVisible(true);
+        for (int i = 0; i < 130; i++) {
+            xPos++;
+            yPos++;
+            drawPanel.repaint();
+            try {
+                TimeUnit.MILLISECONDS.sleep(50);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    class MyDrawPanel extends JPanel {
+        public void paintComponent(Graphics g) {
+            g.setColor(Color.blue);
+            // 在電腦畫畫的世界x, y坐標軸是長這樣
+//            |------> x
+//            |
+//            v
+//            y
+            //可以參考xycoordinate.png
+            g.fillRect(-100,200, getWidth(), getHeight());
+            g.setColor(Color.green);
+            g.fillOval(xPos, yPos, 40, 40);
+        }
+    }
+} 
