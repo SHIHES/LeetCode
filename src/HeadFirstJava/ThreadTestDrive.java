dede51f72b495a23f098e3f9530c0f6e7a32ee0f
SHIHS <stevenshih@25sprout.com>
2023-02-21 17:45:27 +0800

    HeadFirstJava: 增加了ThreadTestDrive 註解

--- src/HeadFirstJava/ThreadTestDrive.java
+++ src/HeadFirstJava/ThreadTestDrive.java
@@ -3,12 +3,41 @@ package HeadFirstJava;
 import java.util.concurrent.TimeUnit;
 
 public class ThreadTestDrive {
+
+    /**
+     * ouput 結果會是
+     * main
+     * main
+     * main
+     *
+     * dumpstack exception msg
+     * dumpstack exception msg
+     * dumpstack exception msg
+     *
+     * top...
+     * top...
+     * top...
+     *
+     * dumpstack exception msg
+     * dumpstack exception msg
+     * dumpstack exception msg
+     *
+     * 每個thread的執行是根據method stack 來決定，因此即使test function已執行完再執行下一個test function了
+     * 對於子線程來說是沒有結束的，因為子線程堆疊的method stack 是 Dummy.run()，並沒有包含test function
+     * @param args
+     */
     public static void main(String[] args) {
         ThreadTestDrive ttd = new ThreadTestDrive();
 
-        for (int i = 0; i < 10; i++) {
-            ttd.test();
-        }
+        ttd.test();
+        ttd.test();
+        ttd.test();
+        ttd.test();
+        ttd.test();
+        ttd.test();
+        ttd.test();
+        ttd.test();
+        ttd.test();
     }
 
     public void test() {
@@ -18,7 +47,9 @@ public class ThreadTestDrive {
 //        System.out.println("back in main");
 //        executor.shutdown();
 
-        Thread myThread = new Thread(() -> sleepThenPrint());
+
+        Runnable runnable = new Dummy();
+        Thread myThread = new Thread(runnable);
         myThread.start();
         System.out.println(Thread.currentThread().getName());
         Thread.dumpStack();
@@ -32,5 +63,18 @@ public class ThreadTestDrive {
         }
         System.out.println("top o’ the stack");
         Thread.dumpStack();
+    }
+
+    static class Dummy implements Runnable{
+        @Override
+        public void run() {
+            try {
+                TimeUnit.SECONDS.sleep(2);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("top o’ the stack");
+            Thread.dumpStack();
+        }
     }
 }
