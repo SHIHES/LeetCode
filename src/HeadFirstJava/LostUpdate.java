846e57bde925f5ea88697deba8e2061e4c129ba7
SHIHS <stevenshih@25sprout.com>
2023-02-22 00:24:36 +0800

    HeadFirstJava: P.652

---
+++ src/HeadFirstJava/LostUpdate.java
@@ -0,0 +1,29 @@
+package HeadFirstJava;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+public class LostUpdate {
+    public static void main(String[] args) throws InterruptedException {
+        ExecutorService pool = Executors.newFixedThreadPool(6);
+        Balance balance = new Balance();
+        for (int i = 0; i < 1000; i++) {
+            pool.execute(() -> balance.increment());
+
+        }
+        pool.shutdown();
+        if (pool.awaitTermination(1, TimeUnit.MINUTES)) {
+            System.out.println("balance = " + balance.balance);
+        }
+    }
+}
+class Balance {
+    int balance = 0;
+    // 正確是要加上 sychronized
+    public  void increment() {
+        balance++;
+        // 超神奇，加上print thread 都可以得到1000 OAO
+//        System.out.println(Thread.currentThread().getName());
+    }
+}
