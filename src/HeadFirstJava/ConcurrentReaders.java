925e618d89b5424bd941965bd929a9d871a0c538
SHIHS <stevenshih@25sprout.com>
2023-02-22 12:30:36 +0800

    HeadFirstJava: P.664 CopyOnWriteArrayList

--- src/HeadFirstJava/ConcurrentReaders.java
+++ src/HeadFirstJava/ConcurrentReaders.java
@@ -3,6 +3,7 @@ package HeadFirstJava;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -11,7 +12,7 @@ import static java.time.format.FormatStyle.MEDIUM;
 
 public class ConcurrentReaders {
     public static void main(String[] args) {
-        List<Chat> chatHistory = new ArrayList<>();
+        List<Chat> chatHistory = new CopyOnWriteArrayList<>();
         ExecutorService executor = Executors.newFixedThreadPool(3);
         for (int i = 0; i < 5; i++) {
             executor.execute(() -> chatHistory.add(new Chat("Hi there!")));
