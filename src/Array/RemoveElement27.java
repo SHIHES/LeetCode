b36f57d1ff8909972a403bf55046869c4e1cc7c6
SHIHS <stevenshih@25sprout.com>
2023-03-01 21:19:59 +0800

    LeetCode(#27 RemoveElement27): Solved the problem

---
+++ src/Array/RemoveElement27.java
@@ -0,0 +1,18 @@
+package Array;
+
+public class RemoveElement27 {
+    static int solution(int[] nums, int target) {
+        // 用兩個指針來解，第一個指針指可以swap的位置
+        int position = 0;
+
+        // 第二個指針用來遍歷整個數列
+        for(int i = 0; i < nums.length; i++){
+            // 當不相等時，為獨特數字，將他移至數列前方，第一指針++
+            if(nums[i] != target) {
+                nums[position] = nums[i];
+                position++;
+            }
+        }
+        return position;
+    }
+}
---
+++ src/Array/RemoveElement27Test.java
@@ -0,0 +1,43 @@
+package Array;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class RemoveElement27Test {
+
+    @Test
+    public void RemoveElementTest1() {
+        int[] nums = {2, 3};
+        int target = 2;
+        int expected = 1;
+        int actual = RemoveElement27.solution(nums, target);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void RemoveElementTest2() {
+        int[] nums = {0, 0, 0, 7, 0};
+        int target = 0;
+        int expected = 1;
+        int actual = RemoveElement27.solution(nums, target);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void RemoveElementTest3() {
+        int[] nums = {3, 1, 2, 3, 4, 3, 3};
+        int target = 3;
+        int expected = 3;
+        int actual = RemoveElement27.solution(nums, target);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void RemoveElementTest4() {
+        int[] nums = {2};
+        int target = 2;
+        int expected = 0;
+        int actual = RemoveElement27.solution(nums, target);
+        Assert.assertEquals(expected, actual);
+    }
+}
